3c37d5a458fde9c9d93da7c15838dd6552cb8260
saisourabh <sai.sourabh2@gmail.com>
2024-05-02 20:02:09 +0530

    print

--- src/main/java/guru/springframework/sfgrestbrewery/SfgRestfulBreweryApplication.java
+++ src/main/java/guru/springframework/sfgrestbrewery/SfgRestfulBreweryApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SfgRestfulBreweryApplication {
 
 	public static void main(String[] args) {
+		System.out.println("After Initial Commit printing ");
 		SpringApplication.run(SfgRestfulBreweryApplication.class, args);
 	}
 
