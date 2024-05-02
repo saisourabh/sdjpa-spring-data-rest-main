cc73c5f00a3f3a34767b23d0583157b37213bba3
saisourabh <sai.sourabh2@gmail.com>
2024-05-02 21:03:37 +0530

    183 - Customize URL Path

--- src/main/java/guru/springframework/sfgrestbrewery/repositories/TempRepository.java
+++ src/main/java/guru/springframework/sfgrestbrewery/repositories/TempRepository.java
@@ -4,9 +4,11 @@ import guru.springframework.sfgrestbrewery.domain.Temp;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
 
+@RepositoryRestResource(path = "temp", collectionResourceRel = "temp")
 public interface TempRepository extends JpaRepository<Temp, UUID> {
     Page<Temp> findAllByOrderByIdAsc(Pageable pageable);
 }
